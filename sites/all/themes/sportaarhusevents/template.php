d430566d8435aa523c987433eb1abba21412df21
Brian Stefan Jensen <brstj@aarhus.dk>
2010-09-20 11:30:04 +0200

    Added modules, themes and libraries

---
+++ sites/all/themes/sportaarhusevents/template.php
@@ -0,0 +1,69 @@
+<?php
+
+function sportaarhusevents_preprocess_page(&$vars, $hook) {
+  // Set variables for the logo and site_name.
+  if (!empty($vars['logo'])) {
+    // Return the site_name even when site_name is disabled in theme settings.
+    $vars['logo_alt_text'] = (empty($vars['logo_alt_text']) ? variable_get('site_name', '') : $vars['logo_alt_text']);
+    $vars['site_logo'] = '<a id="site-logo" href="'. $vars['front_page'] .'" title="'. $vars['logo_alt_text'] .'" rel="home"><img src="'. $vars['logo'] .'" alt="'. $vars['logo_alt_text'] .'" /></a>';
+  }
+}
+
+/**
+ * Add current page to breadcrumb
+ */
+function sportaarhusevents_breadcrumb($breadcrumb) {
+  if (!empty($breadcrumb)) {
+    $title = drupal_get_title();
+    if (!empty($title)) {
+      $breadcrumb[]=$title;
+    }
+    return '<div class="breadcrumb">'. implode(' > ', $breadcrumb) .'</div>';
+  }
+}
+
+/**
+ * Implements theme_menu_item_link()
+ */
+function sportaarhusevents_menu_item_link($link) {
+  if (empty($link['localized_options'])) {
+    $link['localized_options'] = array();
+  }
+
+  // If an item is a LOCAL TASK, render it as a tab
+  if ($link['type'] & MENU_IS_LOCAL_TASK) {
+    $link['title'] = '<span class="tab">' . check_plain($link['title']) . '</span>';
+    $link['localized_options']['html'] = TRUE;
+  }
+
+  return l($link['title'], $link['href'], $link['localized_options']);
+}
+
+// 960 ns function
+function ns() {
+  $args = func_get_args();
+  $default = array_shift($args);
+  // Get the type of class, i.e., 'grid', 'pull', 'push', etc.
+  // Also get the default unit for the type to be procesed and returned.
+  list($type, $return_unit) = explode('-', $default);
+
+  // Process the conditions.
+  $flip_states = array('var' => 'int', 'int' => 'var');
+  $state = 'var';
+  foreach ($args as $arg) {
+    if ($state == 'var') {
+      $var_state = !empty($arg);
+    }
+    elseif ($var_state) {
+      $return_unit = $return_unit - $arg;
+    }
+    $state = $flip_states[$state];
+  }
+
+  $output = '';
+  // Anything below a value of 1 is not needed.
+  if ($return_unit > 0) {
+    $output = $type . '-' . $return_unit;
+  }
+  return $output;
+}
