92e81c8f8ec95850b2a717c8f683421787f0f12d
Brian Stefan Jensen <brstj@aarhus.dk>
2010-09-23 14:13:28 +0200

    Added modules, styling and theming

--- sites/all/themes/sportaarhusevents/templates/page/page.tpl.php
+++ sites/all/themes/sportaarhusevents/templates/page/page.tpl.php
@@ -48,59 +48,60 @@
 
     <div id="main" class="clear-block">
 
-      <div id="main-wrapper">
+      <div id="main-wrapper" class="clear-block">
 
-         <?php print $breadcrumb; ?>
+        <?php print $breadcrumb; ?>
 
-          <?php print $messages; ?>
-          <?php print $help; ?>
+        <?php print $messages; ?>
+        <?php print $help; ?>
 
-          <?php if ($left && !$is_front): ?>
-            <div id="sidebar-left" class="column sidebar region grid-5">
-                    <div id="sidebar-left-inner">
-                <?php print $left; ?>
-              </div>
-            </div> <!-- //end #sidebar-left-inner -->
-          <?php endif; ?>
+        <?php if ($left && !$is_front): ?>
+          <div id="sidebar-left" class="column sidebar region grid-5">
+                  <div id="sidebar-left-inner">
+              <?php print $left; ?>
+            </div>
+          </div> <!-- //end #sidebar-left-inner -->
+        <?php endif; ?>
 
-          <div id="content" class="column <?php $is_front ? print 'grid-10' : print 'grid-11' ?> clear-block">
-            <div id="content-inner">
-              <div id="main-content" class="region">
-                <?php if ($tabs): ?>
-                  <div class="tabs clear-block"><?php print $tabs; ?></div>
-                <?php endif; ?>
-                <?php print $content; ?>
-              </div> <!-- /#main-content -->
+        <div id="content" class="column <?php $is_front ? print 'grid-10' : print 'grid-11' ?> clear-block">
+          <div id="content-inner">
+            <div id="main-content" class="region">
+              <?php if ($tabs): ?>
+                <div class="tabs clear-block"><?php print $tabs; ?></div>
+              <?php endif; ?>
+              <?php print $content; ?>
+            </div> <!-- /#main-content -->
 
-            </div> <!-- // #content-inner -->
-          </div> <!-- //#content -->
+          </div> <!-- // #content-inner -->
+        </div> <!-- //#content -->
 
-          <?php if ($right && $is_front): ?>
+        <?php if ($right && $is_front): ?>
           <div id="sidebar-right" class="column sidebar region grid-6">
             <div id="sidebar-right-inner">
               <?php print $right; ?>
             </div>
           </div> <!--//end #sidebar-right -->
+        <?php endif; ?>
+
+        </div> <!-- //end #main-wrapper -->
+
+        <div id="footer-wrapper" class="clear-block">
+          <?php if ($footer_blocks): ?>
+            <div id="footer-blocks" class="clear-block">
+              <?php print $footer_blocks; ?>
+            </div><!-- /#footer-blocks -->
           <?php endif; ?>
 
-          </div> <!-- //end #main-wrapper -->
+          <?php if ($footer): ?>
+            <div id="footer" class="clear-block">
+              <?php print $footer; ?>
+            </div>
+          <?php endif; ?>
+        </div>
+
       </div> <!-- //end #main -->
 
     </div> <!-- /#page -->
-
-    <div id="footer-wrapper" class="container-16">
-      <?php if ($footer_blocks): ?>
-        <div id="footer-blocks" class="clear-block">
-          <?php print $footer_blocks; ?>
-        </div><!-- /#footer-blocks -->
-      <?php endif; ?>
-
-      <?php if ($footer): ?>
-        <div id="footer" class="clear-block">
-          <?php print $footer; ?>
-        </div>
-      <?php endif; ?>
-    </div>
 
   <?php print $closure; ?>
   
