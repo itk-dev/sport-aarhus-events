a2f7bf73ec6a1ff78106691b31ec2de466054813
Brian Stefan Jensen <brstj@aarhus.dk>
2010-10-01 16:05:05 +0200

    Switch on frontpage (needs work)

--- sites/all/themes/sportaarhusevents/scripts/common.js
+++ sites/all/themes/sportaarhusevents/scripts/common.js
@@ -1,32 +1,11 @@
 $(document).ready(function() {  
 
-  // Add example to top search form
-  // Get search form input
-  $('#fuzzysearch-box-form .form-item input.form-text')
-    // Add example function
-    .example(
-      // Get input field
-      $('#block-fuzzysearch-0 h2.title')
-      // Get the title of the input field
-      .text()
-  );
+  var $items = $('.view-featured-content .field-featured-image-fid a');
 
-  // Hijack carousel pager link
-  $('.views-slideshow-ddblock-cycle-urbanmediaspace a.pager-link').click(function() {
-    location.href = $(this).attr('href');
-  });
-
-  // Add opacity hover effect on footer logos
-  $('#footer-logos div a')
-  .css({
-    'opacity': .25,
-    'display': 'inline-block'
-  })
-  .mouseover(function() {
-    $(this).css('opacity',1);
-  })
-  .mouseout(function() {
-    $(this).css('opacity',.25);
-  })
+  for (i=0;i<$items.length;i++) {
+    if ($($items[i]).text()) {
+      //$($items[i]).css('background-image', 'url(' + $($items[i]).text() + ')');
+    }
+  }
 
 });
