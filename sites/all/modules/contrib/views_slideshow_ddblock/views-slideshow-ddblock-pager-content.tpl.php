09750a62cae647b344c772387fd14db35419b74a
Brian Jensen <mail@magnify.dk>
2010-10-07 17:04:56 +0200

    Fixed featured content, added gfx adn styling of featured content

--- sites/all/modules/contrib/views_slideshow_ddblock/views-slideshow-ddblock-pager-content.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: views-slideshow-ddblock-pager-content.tpl.php,v 1.1.2.2 2010/03/21 08:41:08 ppblaauw Exp $
-
-/**
- * @file
- * Dynamic display block module template: pager template
- *
- */
